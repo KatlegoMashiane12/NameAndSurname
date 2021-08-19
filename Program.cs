a35e9574a7edd7d5e6c2bd776cda533ece382d3a
KatlegoMashiane <Katlego.timothy.9@gmail.com>
2021-08-19 03:51:59 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NameAndSurname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your First Name");
+            String FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Last Name");
+            String LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, {1}", FirstName, LastName);
+            //Console.WriteLine(S);
+        }
+    }
+}
